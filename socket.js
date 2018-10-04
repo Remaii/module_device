fb90e1d98eea6bafe4c8f4f6aaaeca8742f1762d
Remy THIDET <remy@cowork.id>
2018-10-04 14:33:53 +0200

    update function

--- socket.js
+++ socket.js
@@ -23,7 +23,7 @@ module.exports = function(c) {
 
 	socket.on('link', function(data) {
 		console.log(data, 'link receive, is for me?', data.device.uniq === c.deviceId ? "yes" : "no");
-		if (data.device.uniq === c.deviceId) {
+		// if (data.device.uniq === c.deviceId) {
 			switch (data.action) {
 				case "SayHi":
 					easyData.room = data.room;
@@ -64,6 +64,6 @@ module.exports = function(c) {
 					socket.emit('connector', easyData);
 					break;
 			}
-		}
+		// }
 	});
 };
