a8dc194709b394a0a5b39c868a9c8e58e9431f8c
Remaii <remaii1819@gmail.com>
2018-12-06 13:52:59 +0100

    gpio update for mode

--- env/index.js
+++ env/index.js
@@ -10,23 +10,15 @@ let deviceConf = { deviceId: "", maj_auto: false };
 try {
 	deviceConf = jsonfile.readFileSync(pathConf);
 } catch (e) {
-<<<<<<< HEAD
-	deviceConf = (() => { jsonfile.writeFile(pathConf, deviceConf); return deviceConf;	})();
-=======
-	deviceConf = (() => { jsonfile.writeFile(pathConf, { deviceId:"" }); return { deviceId:"" };	})();
->>>>>>> master
+	deviceConf = (() => { jsonfile.writeFile(pathConf, { deviceId:"", maj_auto:false }); return { deviceId:"", maj_auto:false };	})();
 }
 
 let all = {
 	frontUrl: "http://" + exec.shellSync("ifconfig | grep 'inet 192' | cut -d: -f2 | awk '{ print $2  }'").stdout + ":" + port,
 	socketUrl: "https://api.remaii.tk",
-<<<<<<< HEAD
-	deviceId: deviceConf.deviceId,
-	maj_auto: deviceConf.maj_auto
-=======
+	maj_auto: deviceConf.maj_auto,
 	device: deviceConf,
 	deviceId: deviceConf.deviceId,
->>>>>>> master
 };
 
 module.exports = all;
